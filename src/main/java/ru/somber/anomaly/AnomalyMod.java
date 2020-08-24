ffa830e9ba4c37b0e57d8ead3076286877a38cea
Somber <sombersoul43@gmail.com>
2020-08-24 05:38:42 +0500

    Прописаны данные мода и аннотация для загрузки мода.

--- src/main/java/ru/somber/anomaly/AnomalyMod.java
+++ src/main/java/ru/somber/anomaly/AnomalyMod.java
@@ -1,17 +1,20 @@
 package ru.somber.anomaly;
 
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+@Mod(modid = AnomalyMod.MOD_ID, name = AnomalyMod.MOD_NAME, version = AnomalyMod.MOD_VERSION)
 public class AnomalyMod {
-    public static final String MOD_ID = "";
-    public static final String MOD_NAME = "";
-    public static final String MOD_VERSION = "";
+    public static final String MOD_ID = "anomaly_mod";
+    public static final String MOD_NAME = "Somber anomaly mod";
+    public static final String MOD_VERSION = "0.0.0";
 
-    @SidedProxy(clientSide = "ru.somber.anomaly.ClientProxy", serverSide = "ru.somber.anomaly.ServerProxy")
-    public CommonProxy proxy;
+    @SidedProxy(clientSide = "ru.somber.anomaly.ClientProxy",
+            serverSide = "ru.somber.anomaly.ServerProxy")
+    public static CommonProxy proxy;
 
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
