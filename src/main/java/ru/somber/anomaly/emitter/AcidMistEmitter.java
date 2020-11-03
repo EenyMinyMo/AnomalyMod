7477a0e5ee807014b1a6161539cf6721052b8a0d
SomberPC <sombersoul43@gmail.com>
2020-11-03 11:41:37 +0500

    Добавлены частицы для кислотного тумана. Частицы добавлены в эмиттер кислотного тумана.

--- src/main/java/ru/somber/anomaly/emitter/AcidMistEmitter.java
+++ src/main/java/ru/somber/anomaly/emitter/AcidMistEmitter.java
@@ -1,6 +1,9 @@
 package ru.somber.anomaly.emitter;
 
+import ru.somber.anomaly.particle.acidmist.AcidMistBigFogParticle;
+import ru.somber.anomaly.particle.acidmist.AcidMistSmallFogParticle;
 import ru.somber.commonutil.SomberCommonUtil;
+import ru.somber.particlesystem.particle.IParticle;
 
 import java.util.Random;
 
@@ -26,8 +29,51 @@ public class AcidMistEmitter extends AbstractAnomalyEmitter {
 
         Random randomizer = SomberCommonUtil.RANDOMIZER;
 
+        if (getTick() % 18 == 1) {
+            createBigFogParticle();
+        }
+
+        if (getTick() % 16 == 1) {
+            createSmallFogParticle();
+        }
     }
 
+
+    private void createBigFogParticle() {
+        Random randomizer = SomberCommonUtil.RANDOMIZER;
+
+        float x = getPositionX() + randomizer.nextFloat() * 3F - 1.5F;
+        float y = getPositionY() + 0.3F;
+        float z = getPositionZ() + randomizer.nextFloat() * 3F - 1.5F;
+
+        IParticle particle = new AcidMistBigFogParticle(x, y, z);
+
+        addParticleInEmitter(particle);
+        addParticleContainer(particle);
+
+        particle = new AcidMistBigFogParticle(x - 0.4F, y + 0.3F, z + 0.3F);
+
+        addParticleInEmitter(particle);
+        addParticleContainer(particle);
+
+        particle = new AcidMistBigFogParticle(x + 0.35F, y - 0.1F, z + 0.1F);
+
+        addParticleInEmitter(particle);
+        addParticleContainer(particle);
+    }
+
+    private void createSmallFogParticle() {
+        Random randomizer = SomberCommonUtil.RANDOMIZER;
+
+        float x = getPositionX() + randomizer.nextFloat() * 3F - 1.5F;
+        float y = getPositionY() + 0.3F;
+        float z = getPositionZ() + randomizer.nextFloat() * 3F - 1.5F;
+
+        IParticle particle = new AcidMistSmallFogParticle(x, y, z);
+
+        addParticleInEmitter(particle);
+        addParticleContainer(particle);
+    }
 
 }
 
---
+++ src/main/java/ru/somber/anomaly/particle/acidmist/AcidMistSmallFogParticle.java
@@ -0,0 +1,51 @@
+package ru.somber.anomaly.particle.acidmist;
+
+import org.lwjgl.util.vector.Vector3f;
+import ru.somber.anomaly.ParticleIcons;
+import ru.somber.commonutil.SomberCommonUtil;
+import ru.somber.particlesystem.particle.AbstractParticleSimpleData;
+
+import java.util.Random;
+
+public class AcidMistSmallFogParticle extends AbstractParticleSimpleData {
+
+    private final float maxHeight;
+    private final float maxAlpha;
+
+
+    public AcidMistSmallFogParticle(float x, float y, float z) {
+        super(x, y, z, 130 + SomberCommonUtil.RANDOMIZER.nextInt(5), ParticleIcons.smoke4Icon);
+
+        Random randomizer = SomberCommonUtil.RANDOMIZER;
+
+        this.maxHeight = 0.7F + randomizer.nextFloat() * 1.4F;
+        this.maxAlpha = 0.03F;
+
+        setColorFactor(0.3F, 1F, 0.3F, maxAlpha);
+        setHalfSizes(0.6F, 0.6F);
+        setRotateAnglesZ(360 * randomizer.nextFloat());
+        setBlendFactor(1F);
+    }
+
+    public AcidMistSmallFogParticle(Vector3f newPosition) {
+        this(newPosition.getX(), newPosition.getY(), newPosition.getZ());
+    }
+
+
+    @Override
+    public void computeNormalVector(Vector3f destination, float lookAtX, float lookAtY, float lookAtZ, float interpolateFactor) {
+        super.computeNormalVectorSphericalParticle(destination, lookAtX, lookAtY, lookAtZ, interpolateFactor);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+
+        Random randomizer = SomberCommonUtil.RANDOMIZER;
+        float lifeFactor = getLifeFactor() * 2 - 1;
+
+        setPositionY(getPositionY() + maxHeight * (1.0F / getMaxLifeTime()));
+        setAlphaFactor(maxAlpha * (float) (1 - Math.pow(lifeFactor, 2)));
+    }
+
+}
